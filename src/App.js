e5e940ad9d7ea0ea4e40cc7a3e9d29863cb98aff
Sbax <heysbax@protonmail.com>
2022-04-16 09:41:39 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  DnDCharacterSpellSheet,
+  DnDCharacterStatsSheet,
+} from "dnd-character-sheets";
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [character, setCharacter] = useState({});
+
+  const Title = (
+    <img alt="Logo" className="image" src="./officina_coboldi_black_text.svg" />
   );
-}
+
+  const statsSheet = (
+    <DnDCharacterStatsSheet
+      character={character}
+      onCharacterChanged={updateCharacter}
+      title={Title}
+    />
+  );
+
+  const spellSheet = (
+    <DnDCharacterSpellSheet
+      character={character}
+      onCharacterChanged={updateCharacter}
+      title={Title}
+    />
+  );
+
+  function updateCharacter(character) {
+    setCharacter(character);
+  }
+
+  return (
+    <>
+      <header className="no-print header">
+        <button onClick={window.print}>Stampa</button>
+      </header>
+      <div className="app-holder">
+        {statsSheet}
+        <div className="page-break" />
+        {spellSheet}
+      </div>
+    </>
+  );
+};
 
 export default App;
